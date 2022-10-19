3cce76e4abb266dd2ef834c2fb170362f772944d
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-18 21:32:28 -0700

    Add files via upload

---
+++ cpp2.cpp
@@ -0,0 +1,15 @@
+
+// input from user and perform addition
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout << "enter value of a and b : ";
+    cin >> a >> b;
+    c = a+b;
+    cout << "addtion is "<< c;
+}
