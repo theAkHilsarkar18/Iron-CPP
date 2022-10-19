74deb8bccb2805d8141c402178ba7f19b20303cb
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-19 06:31:28 -0700

    Add files via upload

---
+++ cpp3.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+using namespace std;
+
+main()
+{
+	char a[100];
+	cout << "enter a string : ";
+	cin >> a;
+//	printf("%s",a);
+
+// length of string
+	
+	int i,j,len=0,count;
+	
+	for(i=0; a[i]!='\0'; i++)
+	{
+		len ++;
+	}
+//	printf("length : %d",len);
+	
+	for(i=0; i<len; i++)
+	{
+		if(a[i]>=47 && a[i]<=57)
+		{
+			count = 1;
+			break;
+		}
+		else
+		{
+			count = 0;
+		}
+	}
+	
+	if(count==0)
+	{
+		cout << "string is not numeric";
+	}
+	else
+	{
+		cout << "string is numeric";
+	}
+
+	
+	
+}
