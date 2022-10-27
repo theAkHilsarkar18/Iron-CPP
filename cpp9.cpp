55ce63a235e0c1e39b7b46400ccfebb2a9dca7b3
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-27 13:51:19 +0530

    Create cpp9.cpp

---
+++ cpp9.cpp
@@ -0,0 +1,48 @@
+
+// under construction
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// 1. WAP to swap elements of two integer arrays using user define function.
+	
+	
+	cout << "enter length of array : ";
+	int len;
+	cin >> len;
+	int a[len];
+	
+	int i,j;
+	
+	cout << "enter element of array 1 : ";
+	for(i=0; i<len; i++)
+	{
+		cin >> a[i];
+	}
+	
+	
+	
+	int b[len];
+	cout << "enter element of array 2 : ";
+	for(i=0; i<len; i++)
+	{
+		cin >> b[i];
+	}
+	
+	for(i=0; i<len; i++)
+	{
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+}
