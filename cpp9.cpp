d31f2d68b93e11c436a44db31ff273659ca8c28e
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-30 13:00:22 +0530

    Update cpp9.cpp

--- cpp9.cpp
+++ cpp9.cpp
@@ -1,48 +1,52 @@
 
-// under construction
 
 
+// 1. WAP to swap elements of two integer arrays using user define function.
 
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	// 1. WAP to swap elements of two integer arrays using user define function.
-	
-	
-	cout << "enter length of array : ";
-	int len;
+	int len,i,j;
+	cout << "enter lenght of array : ";
 	cin >> len;
-	int a[len];
 	
-	int i,j;
+	int a[len],b[len],c[len];
 	
-	cout << "enter element of array 1 : ";
+	cout << "enter element of array 1 : " << "\n";
 	for(i=0; i<len; i++)
 	{
 		cin >> a[i];
 	}
 	
-	
-	
-	int b[len];
-	cout << "enter element of array 2 : ";
+	cout << "enter element of array 2 : " << "\n";
 	for(i=0; i<len; i++)
 	{
 		cin >> b[i];
 	}
 	
-	for(i=0; i<len; i++)
+	
+    cout << "element of array 1 : \n";
+    for(i=0; i<len; i++)
 	{
+		a[i] = a[i] + b[i];
+		b[i] = a[i] - b[i];
+		a[i] = a[i] - b[i];
 		
+		cout << a[i];
+		cout << "\n";
 	}
 	
-
-	
-	
-	
-	
-	
-	
+	cout << "element of array 2 : \n";
+	for(i=0; i<len; i++)
+	{
+		a[i] = a[i] + b[i];
+		b[i] = a[i] - b[i];
+		a[i] = a[i] - b[i];
+		
+		cout << a[i];
+		cout << "\n";
+	}
+    
 }
