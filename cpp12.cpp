db38cdb9364464b74654bc8a038ce88f5d5e99c4
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-31 23:07:39 -0700

    Update cpp12.cpp

--- cpp12.cpp
+++ cpp12.cpp
@@ -1,4 +1,5 @@
 
+
 // 1. WAP to create a class to read and add two distance. (e.g. 8 feet 16 inch + 4 feet 14 inch = 14 feet 6 inch)
 
 #include<iostream>
@@ -24,34 +25,42 @@ class Finch
 
     }
 
+    void calc()
+    {
+        Finch a1,a2;
+
+        int ans1 = a1.fin();
+        int ans2 = a2.fin();
+        int ans  = ans1 + ans2;
+
+        cout << ans << endl;
     
+       // 1 feet = 12 inch
+
+        int i=0;
+        do
+        {
+            ans = ans - 12;
+            i++;
+
+        } while (ans>=12);
+
+        cout << "feet : " << i << " inch : " << ans;
+    
+
+
+    }
+
+
 
 
 };
 
 int main()
 {
-    Finch a1,a2;
-
-    int ans1 = a1.fin();
-    int ans2 = a1.fin();
-    int ans = ans1 + ans2;
-
-    cout << ans << endl;
     
-    // 1 feet = 12 inch
+   Finch final;
 
-    int i=0;
-    do
-    {
-        ans = ans - 12;
-        i++;
-    } while (ans>=12);
-
-    cout << "feet : " << i << " inch : " << ans;
-    
-
+   final.calc();
    
-   
-
 }
