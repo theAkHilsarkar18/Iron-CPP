ef16fc9b1ef42df66d9ac6f07856b9d4bba26d53
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-01 22:59:36 -0700

    Update oopcpp12.cpp

--- oopcpp12.cpp
+++ oopcpp12.cpp
@@ -1,38 +1,116 @@
+
+// 1. WAP to create a class to read and add two distance. (e.g. 8 feet 16 inch + 4 feet 14 inch = 14 feet 6 inch)
+
 #include<iostream>
 using namespace std;
-class Dis
-{
-    public:
 
-    int feet,inch;
+class Finch
+{
+    public : 
+
+    int fit,in;
 
     void read()
     {
-        cout<<"Enter the feet & Inch"<<endl;
-        cin>>feet>>inch;
+        cout << "enter value of feet and inch : ";
+        cin >> fit >> in;
+
     }
 
-    void result(Dis d2)
-    {   
-        Dis d3;
+    void result(Finch a2)
+    {
+        Finch a3;
+        int feet,inch;
 
-        d3.feet = feet+d2.feet;
-        d3.inch = inch + d2.inch;
+        a3.fit = fit + a2.fit;
+        a3.in = in + a2.in;
 
-        if(d3.inch>=12)
+        if(a3.in >= 12)
         {
-            int f1 = d3.inch/12;
-            d3.inch = d3.inch - (12*f1);
-            d3.feet = f1+d3.feet;
+            feet = a3.in/12;
+            inch = a3.in - (12*feet);
+            feet = feet + a3.fit;
+
+            cout << feet << ":" << inch;
         }
-        cout<<"Answer is Feet ="<<d3.feet<<endl<<"Answer is Inch = "<<d3.inch<<endl;
+        else
+        {
+            cout << a3.fit << ":" << a3.in;
+        }
     }
 };
+
 int main()
 {
-    Dis d1,d2;
-    d1.read();
-    d2.read();
-    d1.result(d2);
-    return 0;
+    Finch a1,a2;
+
+    a1.read();
+    a2.read();
+
+    a1.result(a2);
 }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// class Finch
+// {
+//     public :
+
+//     int fin()
+//     {
+//         int f;
+//         cout << "enter value of feet : " << endl;
+//         cin >> f;
+
+//         int in;
+//         cout << "enter value of inch : " << endl;
+//         cin >> in;
+
+//         int add = (f*12) + in;
+
+//         return add;
+
+//     }
+
+//     void calc()
+//     {
+//         Finch a1,a2;
+
+//         int ans1 = a1.fin();
+//         int ans2 = a2.fin();
+//         int ans  = ans1 + ans2;
+
+//         cout << ans << endl;
+    
+//        // 1 feet = 12 inch
+
+//         int i=0;
+//         do
+//         {
+//             ans = ans - 12;
+//             i++;
+
+//         } while (ans>=12);
+
+//         cout << "feet : " << i << " inch : " << ans;
+    
+
+
+//     }
+
+
+
+
+// };
+
+// int main()
+// {
+    
+//    Finch final;
+
+//    final.calc();
+   
+// }
