ce7438aff7c285b62e3416b51b8c7b6dbcad3fb7
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-01 21:45:02 -0700

    Update cpp13.cpp

--- cpp13.cpp
+++ cpp13.cpp
@@ -1,13 +1,12 @@
 
 // 2. WAP to create a class to read and add two times.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Time 
+class Time
 {
-    public :
-
+public:
     int ak()
     {
         int h;
@@ -22,45 +21,42 @@ class Time
         cout << "enter value in second : " << endl;
         cin >> s;
 
-        int second = (h*60*60) + (m*60) + s;
+        int second = (h * 60 * 60) + (m * 60) + s;
 
         return second;
+    }
 
+    void result()
+    {
+        Time a1, a2;
+
+        int t1 = a1.ak();
+        int t2 = a2.ak();
+
+        int sec = t1 + t2;
+
+        int i = 0;
+
+        do
+        {
+            sec = sec - 60;
+            i++;
+        } while (sec >= 60);
+
+        int j = 0;
+
+        do
+        {
+            i = i - 60;
+            j++;
+        } while (i >= 60);
+
+        cout << j << " : " << i << " : " << sec;
     }
 };
 
 int main()
 {
-    Time a1,a2;
-
-    int t1 = a1.ak();
-    int t2 = a2.ak();
-
-    int sec = t1 + t2;
-
-int i=0;
-do
-{
-    sec = sec - 60;
-    i++;
-} while (sec>=60);
-
-
-
-int j = 0;
-do
-{
-    i = i - 60;
-    j++;
-} while (i>=60);
-
-cout << j <<  " : " << i << " : " << sec;
-
-
-
-
+    Time a;
+    a.result();
 }
-
-
-
-
