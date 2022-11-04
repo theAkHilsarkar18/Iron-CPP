037e26ecea992950f0af30c80bbca49b9edf8417
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-03 22:14:10 -0700

    Create cpp17.cpp

---
+++ cpp17.cpp
@@ -0,0 +1,51 @@
+// 2. WAP to create a class which Read and print Class, Student details using Two Classes. (Make two classes, create one classe's obj in another class.)
+
+#include<iostream>
+using namespace std;
+
+class Read
+{
+    public : 
+
+    int roll;
+    char name[100];
+    float per;
+
+    void set()
+    {
+        cout << "\n\n";
+        cout << "enter your roll number : " << endl;
+        cin >> roll;
+        cout << "enter your name : " << endl;
+        cin >> name;
+        cout << "enter your perentage : " << endl;
+        cin >> per;
+
+    }
+};
+
+class Print
+{
+    public :
+
+    void get()
+    {
+        Read h;
+        h.set();
+        cout << "\n\n";
+        cout << "roll number : " << h.roll << endl;
+        cout << "student name : " << h.name << endl;
+        cout << "percentage : " << h.per << endl;
+    }
+
+};   
+
+int main()
+{
+    
+        
+        Print p;
+        p.get();
+    
+   
+}
