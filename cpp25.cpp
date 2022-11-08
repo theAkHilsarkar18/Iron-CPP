b95f99800bb66186c80cc8a93df3ac61379ebc89
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-07 21:11:05 -0800

    Update cpp25.cpp

--- cpp25.cpp
+++ cpp25.cpp
@@ -1,1 +1,27 @@
 
+
+//1. WAP to demonstrate example of default constructor or no argument constructor.
+
+#include<iostream>
+using namespace std;
+
+class Math
+{
+    public : 
+    int a;
+    int b;
+
+    Math()
+    {
+        a = 10;
+        b = 20;
+
+        cout << "addition is : " << a+b << endl;
+
+    }
+};
+
+int main()
+{
+    Math m1;
+}
