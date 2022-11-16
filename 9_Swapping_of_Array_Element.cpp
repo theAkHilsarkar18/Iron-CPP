811d4e7d967ee519c0740b69739147501014bc01
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-16 19:34:34 +0530

    Update 9_Swapping_of_Array_Element.cpp

--- 9_Swapping_of_Array_Element.cpp
+++ 9_Swapping_of_Array_Element.cpp
@@ -3,50 +3,66 @@
 
 // 1. WAP to swap elements of two integer arrays using user define function.
 
+
 #include<iostream>
 using namespace std;
 
+void swap(int len , int a[] , int b[])
+{
+     for(int i=0; i<len; i++)
+    {
+        a[i] = a[i] + b[i];
+        b[i] = a[i] - b[i];
+        a[i] = a[i] - b[i];
+    }
+}
+
 int main()
 {
-	int len,i,j;
-	cout << "enter lenght of array : ";
-	cin >> len;
-	
-	int a[len],b[len],c[len];
-	
-	cout << "enter element of array 1 : " << "\n";
-	for(i=0; i<len; i++)
-	{
-		cin >> a[i];
-	}
-	
-	cout << "enter element of array 2 : " << "\n";
-	for(i=0; i<len; i++)
-	{
-		cin >> b[i];
-	}
-	
-	
-    cout << "element of array 1 : \n";
-    for(i=0; i<len; i++)
-	{
-		a[i] = a[i] + b[i];
-		b[i] = a[i] - b[i];
-		a[i] = a[i] - b[i];
-		
-		cout << a[i];
-		cout << "\n";
-	}
-	
-	cout << "element of array 2 : \n";
-	for(i=0; i<len; i++)
-	{
-		a[i] = a[i] + b[i];
-		b[i] = a[i] - b[i];
-		a[i] = a[i] - b[i];
-		
-		cout << a[i];
-		cout << "\n";
-	}
-    
+
+    cout << "Enter Length of array : ";
+    int len;
+    cin >> len;
+
+    int a[len],b[len],c[len];
+
+    cout << "Enter element of array 1 : " << endl;
+    for(int i=0; i<len; i++)
+    {   
+        cout << "a[" << i <<"] = ";
+        cin >> a[i];
+        cout << endl;
+    }
+
+    cout << "Enter element of array 2 : " << endl;
+    for(int i=0; i<len; i++)
+    {
+        cout << "b[" << i <<"] = ";
+        cin >> b[i];
+        cout << endl;
+    }
+
+    cout << "-------------------------------------------------------------------------------\n\n";
+
+    // for(int i=0; i<len; i++)
+    // {
+    //     a[i] = a[i] + b[i];
+    //     b[i] = a[i] - b[i];
+    //     a[i] = a[i] - b[i];
+    // }
+
+    swap(len,a,b);
+
+
+    for(int i=0; i<len; i++)
+    {
+        cout << "a[" << i <<"] = " << a[i] << endl;
+    }
+
+    for(int i=0; i<len; i++)
+    {
+        cout << "b[" << i <<"] = " << b[i] << endl;
+    }
+
+
 }
