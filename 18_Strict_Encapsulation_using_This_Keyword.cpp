3f291972568f20012d390426b5d1958c39665b5c
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-18 18:26:01 +0530

    Update and rename cpp18.cpp to 18_Strict_Encapsulation_using_This_Keyword.cpp

--- 18_Strict_Encapsulation_using_This_Keyword.cpp
+++ 18_Strict_Encapsulation_using_This_Keyword.cpp
@@ -1,4 +1,6 @@
 
+
+
 // 3. WAP to create a class Hotel with fields like id, name, type, staff_size, room_size, establish_year, address, rating_type and website.
 // Illustrate the use of encapsulation (strict encapsulation) with this keyword.
 
@@ -33,15 +35,16 @@ class Hotel
 
     void get()
     {
-        cout << endl << "user id : " << id << endl;
-        cout << "hotel name : " << name << endl;
-        cout << "type of hotel : " << type << endl;
-        cout << "staff size of hotel : " << staff << endl;
-        cout << "room size in hotel : " << room << endl;
-        cout << "establish year of hotel : " << year << endl;
-        cout << "address of hotel : " << add << endl;
-        cout << "ratting of hotel : " << rating << endl;
-        cout << "website of hotel : " << website << endl;
+        cout << "\n--------------------------------------------------------------------------------------------------------\n";
+        cout << "user id                   : " << id << endl;
+        cout << "hotel name                : " << name << endl;
+        cout << "type of hotel             : " << type << endl;
+        cout << "staff size of hotel       : " << staff << endl;
+        cout << "room size in hotel        : " << room << endl;
+        cout << "establish year of hotel   : " << year << endl;
+        cout << "address of hotel          : " << add << endl;
+        cout << "ratting of hotel          : " << rating << endl;
+        cout << "website of hotel          : " << website << endl;
 
     }
 };
@@ -56,19 +59,19 @@ int main()
     int rating;
     char website[100];
 
-        cout << "enter your id : " << endl;
+        cout << "enter your id                : ";
         cin >> id;
-        cout << "enter your name : " << endl;
+        cout << "enter your name              : ";
         cin >> name;
-        cout << "enter hotel type : " << endl;
+        cout << "enter hotel type             : ";
         cin >> type;
-        cout << "enter staff size , room size : " << endl;
+        cout << "enter staff size , room size : ";
         cin >> staff >> room;
-        cout << "enter establish year : " << endl;
+        cout << "enter establish year         : ";
         cin >> year;
-        cout << "enter rating of hotel : " << endl;
+        cout << "enter rating of hotel        : ";
         cin >> rating;
-        cout << "enter website detail : " << endl;
+        cout << "enter website detail         : ";
         cin >> website;
 
     Hotel h;
