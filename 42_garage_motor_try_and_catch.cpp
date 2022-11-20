8ae32f591b095e019273f416b8c65e8c6dbc7f97
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-11-20 20:08:49 +0530

    Update 42_garage_motor_try_and_catch.cpp

--- 42_garage_motor_try_and_catch.cpp
+++ 42_garage_motor_try_and_catch.cpp
@@ -54,3 +54,66 @@ int main()
 {
     Garage g1;
 }
+
+
+// second method
+
+
+// // 1. Write a "Garage" class that has a "Car" that is having troubles with its "Motor".
+// // Use a function-level try block in the "Garage" class constructor to catch an exception (thrown from the "Motor" class) when its "Car" object is initialized.
+// // Throw a different exception from the body of the "Garage" constructor s handler and catch it in main( ).
+
+// #include <iostream>
+// using namespace std;
+
+// class Motor;
+// class Garage;
+
+// class Motor
+// {
+// public:
+//     int a;
+//     void payment()
+//     {
+//         cout << "Enter payment amount : ";
+//         cin >> a;
+//     }
+// };
+
+// class Garage : public Motor
+// {
+// public:
+//     Garage()
+//     {
+//         payment();
+//         try
+//         {
+//             if (a == 0)
+//             {
+//                 throw a;
+//             }
+//             else
+//             {
+//                 cout << "Car service done !!";
+//             }
+//         }
+//         catch (int a)
+//         {
+//             cout << "Zero amount in payment is not allowed !!";
+//         }
+//     }
+// };
+
+// class Car
+// {
+// public:
+//     Car()
+//     {
+//         Garage g1;
+//     }
+// };
+
+// int main()
+// {
+//     Car c1;
+// }
